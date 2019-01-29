b05adadab34b9131313a10d051bcdf8139452605
Etherspirit <47112784+Etherspirit@users.noreply.github.com>
2019-01-29 10:51:43 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,17 +9,18 @@ package com.example;
  */
 
 public class Greeter {
- /** 
- * This is a Categorie .
- */ 
+   /** 
+    * This is a Categorie .
+    */ 
   
   public Greeter() {
 
   }
- /** 
- * @param  someone name of a person 
- * @return greeting string
- */
+ 
+  /** 
+   * @param  someone name of a person 
+   * @return greeting string
+   */
   
   public final String greet(final String someone) {
     return String.format("Hello maître, %s!", someone);
