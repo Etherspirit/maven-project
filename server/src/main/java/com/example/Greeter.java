22bf4f3e25589a4a7efac7c93461a260a8208df3
Etherspirit <47112784+Etherspirit@users.noreply.github.com>
2019-01-29 10:46:06 +0100

    Add comment
    add comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,22 @@
 package com.example;
 
+/** 
+ * This is a class . 
+ */
 
 public class Greeter {
-
+ /** 
+ * This is a Categorie .
+ */ 
   
   public Greeter() {
 
   }
-
+ /** 
+ * @param  someone name of a person 
+ * @return greeting string
+ */
+  
   public final String greet(final String someone) {
     return String.format("Hello maître, %s!", someone);
   }
