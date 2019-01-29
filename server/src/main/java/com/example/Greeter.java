134aee8c754a45044c1c2b2edaac429285d97ccc
Etherspirit <47112784+Etherspirit@users.noreply.github.com>
2019-01-29 10:55:10 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,18 +1,16 @@
 /**
  * Javadoc du package .
  */
-
 package com.example;
 
 /** 
  * This is a class . 
  */
-
 public class Greeter {
+   
    /** 
-    * This is a Categorie .
-    */ 
-  
+    * This is a Constructor .
+    */
   public Greeter() {
 
   }
@@ -21,7 +19,6 @@ public class Greeter {
    * @param  someone name of a person 
    * @return greeting string
    */
-  
   public final String greet(final String someone) {
     return String.format("Hello maître, %s!", someone);
   }
